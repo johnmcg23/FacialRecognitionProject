ad4d8e1b6e3f913c5f84a5da87353ba37584bbb7
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-17 10:30:26 +0000

    passing argument username through to main method

--- Controller/SignUpController.py
+++ Controller/SignUpController.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from urllib import request
+
+from flask import Flask, jsonify
 import threading
 
 from SignUpFaceId import main
@@ -6,11 +8,15 @@ from SignUpFaceId import main
 app = Flask(__name__)
 
 
-@app.route('/signup/faceid', methods=['GET', 'POST'])
-def PostFaceImageToS3Bucket():
+@app.route('/signup/faceid', methods=['POST'])
+def runFacialRecognitionSignUp():
+    # Get the username from the request
+    username = request.json['username']
+
     # Start the face recognition in a new thread
-    threading.Thread(target=main).start()
-    return 'Face recognition started!'
+    threading.Thread(target=main, args=(username,)).start()
+
+    return jsonify({'message': 'Please wait while we scan your face...'})
 
 
 if __name__ == '__main__':
