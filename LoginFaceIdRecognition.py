dfb51bf5360bcac6205a13cc6876001ff5fd3914
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-17 15:48:26 +0000

    Removal of unessecary code

--- LoginFaceIdRecognition.py
+++ LoginFaceIdRecognition.py
@@ -7,61 +7,6 @@ import requests
 import base64
 import io
 
-
-def load_known_faces(username):
-    known_faces = []
-    known_face_names = []
-    # Use GitHub API to get the content of the repository
-    url = f"https://api.github.com/repos/{username}/repo/contents/"
-    response = requests.get(url)
-    files = response.json()
-
-    for file in files:
-        if file['name'].endswith((".png", ".jpg", ".jpeg")):
-            # Download the image file
-            image_url = file['download_url']
-            response = requests.get(image_url)
-            image_data = response.content
-
-            # Convert the image data to an image
-            image = Image.open(io.BytesIO(image_data))
-
-            face_encoding = face_recognition.face_encodings(image)[0]
-            known_faces.append(face_encoding)
-            name, ext = os.path.splitext(file['name'])
-            known_face_names.append(name)
-    return known_faces, known_face_names
-
-
-def process_frame(frame, known_faces, known_face_names):
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(small_frame)
-    face_encodings = face_recognition.face_encodings(small_frame, face_locations)
-
-    face_names = []
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_faces, face_encoding)
-        name = "NO MATCH"
-        print('no match')
-        color = (0, 0, 255)  # Red
-
-        # If a match was found in known_faces, use the first one.
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
-
-            print('match')
-            color = (0, 255, 0)  # Green
-
-        face_names.append((name, color))
-
-    return face_locations, face_names
-
-
 def display_results(frame, face_locations, face_names):
     # Display the results
     for (top, right, bottom, left), (name, color) in zip(face_locations, face_names):
