44c7812f936a58c141a155c1e19574736a5b9382
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-18 12:07:27 +0000

    Login functional

--- trueApplication.py
+++ trueApplication.py
@@ -6,24 +6,35 @@ from SignUpFaceId import main as signup_main
 
 app = Flask(__name__)
 
+from concurrent.futures import ThreadPoolExecutor
 
-@app.route('/login/faceid/<username>', methods=['POST', 'GET'])
+# Create a ThreadPoolExecutor
+executor = ThreadPoolExecutor(max_workers=1)
+
+
+@app.route('/login/faceid/<username>', methods=['GET'])
 def runFacialRecognitionLogin(username):
     print("Gets in method in python")
 
-    # Start the face recognition in a new thread
-    threading.Thread(target=login_main, args=(username,)).start()
+    # Start the face recognition in a new thread and get a Future object
+    future = executor.submit(login_main, username)
 
-    return jsonify('message: Please wait while we scan your face...', 'result')
+    # Get the result from the Future
+    result = future.result()
+
+    # Return the result
+    if result:
+        return jsonify(True)
+    else:
+        return jsonify(False)
 
 
-@app.route('/signup/faceid/<username>', methods=['GET', 'POST'])
+@app.route('/signup/faceid/<username>', methods=['POST'])
 def runFacialRecognitionSignUp(username):
     # Start the face recognition in a new thread
     threading.Thread(target=signup_main, args=(username,)).start()
 
-    return jsonify({'message': 'Please wait while we scan your face...'})
-
+    return jsonify(True)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3000, debug=True)
+    app.run(host='localhost', port=3001, debug=True)
